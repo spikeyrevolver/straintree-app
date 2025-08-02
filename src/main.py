2fccd6ede71d2213f21dce2d6b0821968ef52e44
spikeyrevolver <thomas.sealock@gmail.com>
2025-08-02 18:06:37 -0500

    Update main.py
    1.7 currently broken

--- src/main.py
+++ src/main.py
@@ -101,3 +101,15 @@ def serve_spa(path):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+from flask import send_from_directory
+import os
+
+# Add this route to serve React app
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join('dist', path)):
+        return send_from_directory('dist', path)
+    else:
+        return send_from_directory('dist', 'index.html')
